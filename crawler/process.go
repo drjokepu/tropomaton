ec477608d067fea48931a80161fb0b6ee806ef86
Tamas Czinege <tomi.czinege@gmail.com>
2014-11-16 03:04:34 +0000

    Revert "print curenntly processed URL"
    This reverts commit 8f29785a749310c0564649a8ba68befed794e335.

--- crawler/process.go
+++ crawler/process.go
@@ -1,10 +1,6 @@
 package main
 
-import "fmt"
-
 func process(url string, getAllLinks bool) error {
-	fmt.Printf("%v\n", url)
-
 	htmlChannel := make(chan string)
 	errorChannel := make(chan error)
 	go fetch(url, htmlChannel, errorChannel)
